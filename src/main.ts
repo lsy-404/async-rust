324d0fa9179ddf493caa3cd3933c46f04ae7f0d5
Wuyilingwei <53390868+wuyilingwei@users.noreply.github.com>
2026-09-08 10:28:28 -0400

    feat: build local classroom workbench

---
+++ src/main.ts
@@ -0,0 +1,7 @@
+import { createApp } from "vue";
+import "@platform-kit/fluent/style.css";
+import "@model-auth/vue/style.css";
+import "./style.css";
+import App from "./App.vue";
+
+createApp(App).mount("#app");
